05b041556e91108238630135b0bdad28c8d5bb7c
Timur <64526441+SafiullinT@users.noreply.github.com>
2024-09-24 14:23:28 +0300

    Initial commit

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -3,10 +3,10 @@ class Car {
   final String imagePath;
   final double price;
   final String description;
-  final String power; // Мощность двигателя
-  final String acceleration; // Разгон до 100 км/ч
-  final String engineType; // Тип двигателя
-  final String topSpeed; // Максимальная скорость
+  final String power;
+  final String acceleration; 
+  final String engineType; 
+  final String topSpeed; 
 
   Car(this.name, this.imagePath, this.price, this.description, this.power, this.acceleration, this.engineType, this.topSpeed);
 }
