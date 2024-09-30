216ab97299cd312f35e1ec5993c9299da6b66270
Timur <64526441+SafiullinT@users.noreply.github.com>
2024-10-01 01:52:27 +0300

    Update car.dart

--- lib/models/car.dart
+++ lib/models/car.dart
@@ -1,12 +1,21 @@
 class Car {
   final String name;
-  final String imagePath;
+  final String imageUrl;
   final double price;
   final String description;
-  final String power; // Мощность двигателя
-  final String acceleration; // Разгон до 100 км/ч
-  final String engineType; // Тип двигателя
-  final String topSpeed; // Максимальная скорость
+  final String horsepower;
+  final String acceleration;
+  final String engineType;
+  final String maxSpeed;
 
-  Car(this.name, this.imagePath, this.price, this.description, this.power, this.acceleration, this.engineType, this.topSpeed);
+  Car(
+      this.name,
+      this.imageUrl,
+      this.price,
+      this.description,
+      this.horsepower,
+      this.acceleration,
+      this.engineType,
+      this.maxSpeed,
+      );
 }
