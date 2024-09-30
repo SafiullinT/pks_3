5e31c03fcb727a8be61c29f10e9945f6cf7e5e29
Timur <64526441+SafiullinT@users.noreply.github.com>
2024-10-01 01:52:57 +0300

    Initial commit

--- lib/components/car_card.dart
+++ lib/components/car_card.dart
@@ -4,30 +4,39 @@ import '../pages/car_detail_screen.dart';
 
 class CarCard extends StatelessWidget {
   final Car car;
+  final VoidCallback onDeleteCar;
 
-  CarCard({required this.car});
+  CarCard({required this.car, required this.onDeleteCar});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
-      child: ListTile(
-        leading: Image.asset(
-            car.imagePath,
-            width: 100,
-            height: 100,
-            fit: BoxFit.cover
-        ),
-        title: Text(car.name),
-        subtitle: Text('${car.price} \$'),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CarDetailScreen(car: car),
-            ),
-          );
-        },
+      child: Column(
+        children: [
+          ListTile(
+            leading: Image.network(car.imageUrl, width: 100),
+            title: Text(car.name),
+            subtitle: Text('${car.price} \$'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CarDetailScreen(car: car),
+                ),
+              );
+            },
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: onDeleteCar,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
