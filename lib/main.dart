7dd704af026126c83c9545f6e3eec23bd8a7448b
Timur <64526441+SafiullinT@users.noreply.github.com>
2024-10-01 01:49:39 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 import 'pages/car_store_screen.dart';
+import 'pages/add_car_screen.dart';
+import 'models/car.dart';
 
 void main() {
   runApp(CarStoreApp());
 }
 
-class CarStoreApp extends StatelessWidget {
+class CarStoreApp extends StatefulWidget {
+  @override
+  _CarStoreAppState createState() => _CarStoreAppState();
+}
+
+class _CarStoreAppState extends State<CarStoreApp> {
+  List<Car> cars = [
+    Car(
+      'Tesla Model S',
+      'https://www.zr.ru/d/story/c4/924100/tesla-model-s-samyj-dalnobojnyj-elektromobil.jpg',
+      79999,
+      'Электрический седан с невероятным запасом хода и высокой производительностью.',
+      '1020 л.с.',
+      '2.1 сек до 100 км/ч',
+      'Электрический',
+      '322 км/ч',
+    ),
+
+  ];
+
+  void addCar(Car newCar) {
+    setState(() {
+      cars.add(newCar);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CarStore',
+      title: 'Car Store',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CarStoreScreen(),
+      home: CarStoreScreen(cars: cars, addCar: addCar),
     );
   }
 }
